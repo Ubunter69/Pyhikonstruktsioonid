a542c13bacf219b5f481862aa6343e03b9b6e08f
opilane <opilane@HP-CZC2349HTM>
2025-01-20 11:26:33 +0200

    print,input()funktsioonid 20.01.25

--- Pyhikonstruktsioonid.py
+++ Pyhikonstruktsioonid.py
@@ -1,3 +1,43 @@
-﻿
+
 print("Hello World! 1","Hello World! 2",end="...", sep="---") #sep-сепаратор который разделяет между собой какие то слова
 #(#-какой либо комментарий)
+# andmetüübid 
+# täisarv- int, ujukomaarv - float, sõne - str, tõeväärtused - bool
+#aastendamine **
+print()
+a=5
+aste=2
+tulemus=a**aste 
+print(a, "**",aste,"=",tulemus)
+#korrutamine * 
+# arv1=int(input("Esimene korrutaja: "))
+# arv2=int(input("Teine korrutaja: "))
+# tulemus=arv1*arv2
+# print(arv1, "*",arv2,"=",tulemus)
+#jagamine / 
+# arv1=int(input("Esimene korrutaja: "))
+# arv2=int(input("jagaja: "))
+# tulemus=arv1/arv2
+# print(arv1, "/",arv2,"=",tulemus)
+# print(type(tulemus)) #функция Type - функция показывает тип данных
+#Jagamisjääk %
+arv1=int(input("Esimene korrutaja: "))
+arv2=int(input("Jagaja: "))
+tulemus=arv1%arv2
+print(arv1, "%",arv2,"=",tulemus)
+#Liitmine + 
+arv1=int(input("Esimene korrutaja: "))
+arv2=int(input("Liitaja: "))
+tulemus=arv1+arv2
+print(arv1, "+",arv2,"=",tulemus)
+#Lahutamine -
+arv1=int(input("Esimene korrutaja: "))
+arv2=int(input("Lahutaja: "))
+tulemus=arv1-arv2
+print(arv1, "-",arv2,"=",tulemus)
+#jagamise täisarvulise  osa leidmine // 
+arv1=int(input("Esimene korrutaja: "))
+arv2=int(input("jagaja: "))
+tulemus=arv1//arv2
+print(arv1, "//",arv2,"=",tulemus)
+
